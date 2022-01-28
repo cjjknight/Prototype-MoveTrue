e0d3bdace1a4b7fa0d073f2bb95889af9af17d20
Chris Johnson <Christopher.Johnson42@gmail.com>
2022-01-28 11:40:13 -0600

    -Add ContentModel and make it observable object and append it to home view

--- ProtoType-MoveTrue/ProtoType_MoveTrueApp.swift
+++ ProtoType-MoveTrue/ProtoType_MoveTrueApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct ProtoType_MoveTrueApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ ProtoType-MoveTrue/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  ProtoType-MoveTrue
+//
+//  Created by user210522 on 1/28/22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
