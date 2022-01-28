dd1d82e4ed197d667b3230846508dc9748113fd3
Chris Johnson <Christopher.Johnson42@gmail.com>
2022-01-28 11:27:42 -0600

    Initial Commit

---
+++ ProtoType-MoveTrue/ProtoType_MoveTrueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoType_MoveTrueApp.swift
+//  ProtoType-MoveTrue
+//
+//  Created by user210522 on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ProtoType_MoveTrueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
